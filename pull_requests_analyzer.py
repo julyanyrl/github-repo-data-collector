6b392067cf392ee1ee6f37691e8a3552bee312e2
Julyany Rodrigues <167951642+julyanyrl@users.noreply.github.com>
2024-11-01 19:18:10 +0000

    Coleta e analisa pull requests

--- pull_requests_analyzer.py
+++ pull_requests_analyzer.py
@@ -22,38 +22,44 @@ repositorios = [
 ]
 
 def get_pr_statistics(repo, date_limit):
-    url = f'https://api.github.com/repos/{repo}/pulls?state=all'
-    
-    response = requests.get(url, headers=HEADERS)
-    
-    if response.status_code != 200:
-        print(f"Erro ao recuperar PRs do repositório {repo}: {response.status_code}")
-        print("Verifique o nome do repositório e o token de autenticação.")
-        return 0, 0, 0, 0
-    
-    prs = response.json()
     open_prs = 0
     total_no_response_time = 0
     accepted_prs = 0
     rejected_prs = 0
-    
-    for pr in prs:
-        pr_date = datetime.strptime(pr['created_at'], "%Y-%m-%dT%H:%M:%SZ")
+    page = 1
+
+    while True:
+        url = f'https://api.github.com/repos/{repo}/pulls?state=all&per_page=100&page={page}'
+        response = requests.get(url, headers=HEADERS)
         
-        if pr_date > date_limit:
-            continue
-        
-        if pr['state'] == 'open':
-            open_prs += 1
+        if response.status_code != 200:
+            print(f"Erro ao recuperar PRs do repositório {repo}: {response.status_code}")
+            print("Verifique o nome do repositório e o token de autenticação.")
+            return 0, 0, 0, 0
+
+        prs = response.json()
+        if not prs:
+            break
+
+        for pr in prs:
+            pr_date = datetime.strptime(pr['created_at'], "%Y-%m-%dT%H:%M:%SZ")
             
-            if not pr.get('comments', 0):
-                no_response_time = (date_limit - pr_date).days
-                total_no_response_time += no_response_time
-        elif pr['merged_at'] is not None:
-            accepted_prs += 1
-        elif pr['closed_at'] is not None and pr['merged_at'] is None:
-            rejected_prs += 1
-    
+            if pr_date > date_limit:
+                continue
+            
+            if pr['state'] == 'open':
+                open_prs += 1
+                
+                if not pr.get('comments', 0):
+                    no_response_time = (date_limit - pr_date).days
+                    total_no_response_time += no_response_time
+            elif pr['merged_at'] is not None and datetime.strptime(pr['merged_at'], "%Y-%m-%dT%H:%M:%SZ") <= date_limit:
+                accepted_prs += 1
+            elif pr['closed_at'] is not None and pr['merged_at'] is None and datetime.strptime(pr['closed_at'], "%Y-%m-%dT%H:%M:%SZ") <= date_limit:
+                rejected_prs += 1
+
+        page += 1
+
     avg_no_response_time = total_no_response_time / open_prs if open_prs > 0 else 0
     
     return open_prs, avg_no_response_time, accepted_prs, rejected_prs
